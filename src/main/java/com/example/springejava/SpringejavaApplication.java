c7dc9526fe53acf692b585a4e4007525052833ec
ravi9607 <ravi2018cs177abesit@gmail.com>
2021-04-14 14:49:28 +0530

    maven plugin

--- src/main/java/com/example/springejava/SpringejavaApplication.java
+++ src/main/java/com/example/springejava/SpringejavaApplication.java
@@ -1,6 +1,5 @@
 package com.example.springejava;
 
-import org.hibernate.dialect.MySQL8Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
