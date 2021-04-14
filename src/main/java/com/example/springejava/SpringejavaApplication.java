cce69530e76e5ed102441e147e4343f55b130b77
ravi9607 <ravi2018cs177abesit@gmail.com>
2021-04-14 14:18:18 +0530

    maven plugin

--- src/main/java/com/example/springejava/SpringejavaApplication.java
+++ src/main/java/com/example/springejava/SpringejavaApplication.java
@@ -3,12 +3,19 @@ package com.example.springejava;
 import org.hibernate.dialect.MySQL8Dialect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringejavaApplication {
+public class SpringejavaApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringejavaApplication.class, args);
+
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringejavaApplication.class);
 
     }
 
